4762972063d06c2be891c378f6d7ba569c491579
edwardg <egarrett3@fordham.edu>
2020-10-06 09:38:44 -0400

    albumShow pg DOM layed out

--- frontend/components/album/album_show_page.js
+++ frontend/components/album/album_show_page.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import FooterItem from "../footer/footer";
+import Greeting from '../greeting/greeting';
+import GreetingContainer from "../greeting/greeting_container";
 
 
 class AlbumShow extends React.Component {
@@ -13,20 +15,54 @@ class AlbumShow extends React.Component {
 
     render() {
         return (
-            <div>
-                <div>
-                    <div>
-
-                    </div>
-                    <div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
+          <div>
+            <GreetingContainer />
+            <div id="song-page">
+              <div id="song-header"></div>
+              <div id="single-track">
+                <div id="music-community">
+                  <div className="song-tab">music</div>
+                  <div className="song-tab">community</div>
                 </div>
-                <FooterItem />
+
+                <div id="song-info">
+                  <div id="album-track-title">
+                    <div id="song-info-title">
+                      <div>{this.props.currentUser.username}</div>
+                      <div>from {this.props.currentUser.username}</div>
+                    </div>
+                    <div id="trackANDalbum">
+                      <div id="musicPlayerTrack">
+                        <div>
+                            
+                        </div>
+                        <div>Digital Track Streaming + Download</div>
+                        <div>
+                          Includes unlimited streaming via the free Bandcamp
+                          app, plus high-quality download in MP3, FLAC and more.
+                          Buy Digital Track €3 EUR or more Send as Gift{" "}
+                        </div>
+                        <div>Buy the Full Digital Album</div>
+                        <div>
+                          from Mousse T´s Classic Remixes Vol. 1, released
+                          September 3, 2020
+                        </div>
+                        <div>all rights reserved</div>
+                      </div>
+                      <div id="album-picture">
+                          <div>
+
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div id="discogrpahy"></div>
+                </div>
+              </div>
             </div>
-        )
+            <FooterItem />
+          </div>
+        );
     }
 }
 
