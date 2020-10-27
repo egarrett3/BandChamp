251a68c9c33dfb11a9c97743aafaed5521f17ff6
edwardg <egarrett3@fordham.edu>
2020-10-27 11:56:31 -0400

    add Link to dailt-item and styled link text

--- frontend/components/bandchampdaily/daily_item.js
+++ frontend/components/bandchampdaily/daily_item.js
@@ -1,47 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const describers = (props) => {
-    switch (props.song.id) {
-        case (1):
-            return 'The song of your dreams'
-        case (2):
-            return 'Who else but the best in their genre'
-        case (3):
-            return 'So good, So raw'
-        case (4):
-            return 'The strongest case for song of the year'
-        case (5):
-            return 'A crowd favorite'
-        case (6):
-            return 'Beauty to my Brains'
-        case (7):
-            return 'A top classic'
-        case (8):
-            return 'A bandchamp favorite'
-        default:
-            return "This band's number one hit"
-    }
-}
 
 const DailyItem = props => {
     
 
     return (
-        <div id='showcase'>
+        <Link id='showcase' to={{
+            pathname: `/songPage/${props.song.id}`
+        }}>
             <img src={props.song.photo_url} id='showcase-pic' />
             <div id="frame">
                 <div>
                     <h2 id='main-title'>{props.descrip}</h2>
-                    <div id='by-title'> by {props.song.title}</div>
-                    <div id='paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna 
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    <div id='by-title'> Title: {props.song.title}</div>
+                    <div id='paragraph'>Cowboy Bebop, one of the most critically 
+                    acclaimed anime, will definitely knock your socks off with its 
+                    unique style of music. One of the best examples of this is 
+                    the show's opening―Tank!
                     </div>
                 </div>
                 <div className="feature">FEATURE</div>
             </div>
-        </div>
+        </Link>
     )
 
 }
