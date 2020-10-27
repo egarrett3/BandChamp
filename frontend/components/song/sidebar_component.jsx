038677c9a8d32d2fc78942a3df4cea5e2358e1d5
edwardg <egarrett3@fordham.edu>
2020-10-27 11:17:06 -0400

    seed data depending on song

--- frontend/components/song/sidebar_component.jsx
+++ frontend/components/song/sidebar_component.jsx
@@ -9,14 +9,38 @@ class SideContainer extends React.Component {
 
     }
   }
+
+  describers() {
+    debugger
+    switch(this.props.song.id) {
+      case(1):
+        return 'The song of your dreams'
+      case(2):
+        return 'Who else but the best in their genre'
+      case(3):
+        return 'So good, So raw'
+      case(4):
+        return 'The strongest case for song of the year'
+      case(5):
+        return 'A crowd favorite'
+      case(6):
+        return 'Beauty to my Brains'
+      case(7):
+        return 'A top classic'
+      case(8):
+        return 'A bandchamp favorite'
+      default:
+        return "This band's number one hit"
+    }
+  }
  
   
   render() {
     return (
       <li className="side-img-container">
         <div className="building-image-words">
-          <div>Lorem Ipsum</div>
-          <div>Veni Vidi Vici. Alia Iacta Est.</div>
+          <div>{this.props.song.title}</div>
+          <div>{this.describers()}</div>
         </div>
         <img
         src={this.props.song.photo_url}
