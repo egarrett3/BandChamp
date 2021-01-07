d89cb4a15a71fe60ac987b9790add4c38377b9d7
edwardg <egarrett3@fordham.edu>
2021-01-07 16:16:13 -0700

    destroy album in albumReducer

--- frontend/reducers/album_reducer.js
+++ frontend/reducers/album_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_ALBUMS, RECEIVE_ALBUM } from "../actions/album_actions";
+import { RECEIVE_ALBUMS,DESTROY_ALBUM, RECEIVE_ALBUM } from "../actions/album_actions";
 import { merge } from "lodash";
 
 const initialState = {};
@@ -10,7 +10,12 @@ const albumReducer = (state = initialState, action) => {
   switch (action.type) {
     case RECEIVE_ALBUM:
       const singleAlb = { [action.album.id]: action.album };
-      return Object.assign({}, state, singleAlb)
+      return Object.assign({}, state, singleAlb);
+    case DESTROY_ALBUM:
+      const id = action.album.id;
+      const albums = Object.values(state);
+      const newAlbumList = albums.filter((album) => album.id !== id);
+      return Object.assign({}, newAlbumList);
     default:
       return state;
   }
