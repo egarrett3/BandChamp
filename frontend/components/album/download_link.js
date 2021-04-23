0947b35490c164122417b23bcdcd855522fda628
edwardg <egarrett3@fordham.edu>
2021-04-22 21:41:40 -0700

    style/position songlist delete and download link

--- frontend/components/album/download_link.js
+++ frontend/components/album/download_link.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import AudioButton from '../audio/audio_button';
 
 class DownloadLink extends React.Component {
     constructor(props) {
